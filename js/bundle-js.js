47cd6ecd8191de5205e38c9bcf406fb1bfd13756
Clyde D'Souza <clyd94@gmail.com>
2018-09-20 19:35:41 +1200

    added basic google analytics event triggers

--- js/bundle-js.js
+++ js/bundle-js.js
@@ -3299,6 +3299,21 @@ function activateReleaseButton() {
     $("#showBuildViewBtn").removeClass("button");
     $("#showBuildViewBtn").addClass("button-inverse");
 }
+
+function sendShareClickData(action, label) {
+    sendEventData('Share', action, label);
+}
+
+function sendCTAClickData(action, label) {
+    sendEventData('CTA', action, label); 
+}
+
+function sendEventData(category, action, label) {
+    gtag('event', action, {
+        'event_category': category,
+        'event_label': label
+    });
+}
 $(document).ready(function () {
    
     let buildJsonUrl = sanityCheckUrl(getUrlVars()[buildJsonUrlQueryStringKey]);
---
+++ js/modules/analytics.js
@@ -0,0 +1,15 @@
+﻿
+function sendShareClickData(action, label) {
+    sendEventData('Share', action, label);
+}
+
+function sendCTAClickData(action, label) {
+    sendEventData('CTA', action, label); 
+}
+
+function sendEventData(category, action, label) {
+    gtag('event', action, {
+        'event_category': category,
+        'event_label': label
+    });
+}
