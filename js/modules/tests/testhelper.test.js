840e625a9d44296aca3a7c044c5b0463d32abdd7
Clyde D'Souza <clyd94@gmail.com>
2018-10-13 12:53:15 +1300

    Another failed test case

--- js/modules/tests/testhelper.test.js
+++ js/modules/tests/testhelper.test.js
@@ -2,5 +2,8 @@
 describe("Test checker", function () {
     it("Returns true", function () {
         expect(true).toBe(false);
-	});
+    });
+    it("Returns another true", function () {
+        expect(true).toBe(false);
+    });
 });
